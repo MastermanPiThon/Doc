bef6b891c0e62c502c1eff507ce2b46c5fecc070
cgkerr27 <cgkerr27@gmail.com>
2014-01-08 23:11:13 -0500

    Class Five notes first draft

---
+++ ClassFive/input.py
@@ -0,0 +1,11 @@
+#Input
+
+something = 42
+
+a = raw_input ("Enter something: ")
+
+print "You entered '{}'".format (a)
+
+b = input ("Enter 'something': ")
+
+print "You entered '{}'".format (b)
---
+++ ClassFive/output.py
@@ -0,0 +1,32 @@
+#Output
+
+print "Hello Python"
+
+print ("Hello Python")
+
+print 'Hello Python'
+
+print "Hello 'Python'"
+
+print 'Hello "Python"'
+
+print "Hello\nPython"
+
+print "Hello Python for the {}th time".format (7)
+
+print "{} {} {}".format ("One", "Two", "Three")
+
+print "{0} {1} {2}".format ("One", "Two", "Three")
+
+print "{One} {Two} {Three}".format (One="A", Two="B", Three="C")
+
+print "{} {} {third}".format (1, 2, third="Third")
+
+
+
+import math
+
+print ("Pi: {}".format(math.pi))
+
+print ("Pi: {0:.5f}".format (math.pi))
+
